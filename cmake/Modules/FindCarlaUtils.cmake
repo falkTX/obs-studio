a2bdde0abc3ac6826dbb500bc255a4b5ba021c19
falkTX <falktx@falktx.com>
2023-07-21 15:35:42 +0200

    Rename CARLAUTILS to CarlaUtils; Set rpath as needed
    Signed-off-by: falkTX <falktx@falktx.com>

--- cmake/Modules/FindCarlaUtils.cmake
+++ cmake/Modules/FindCarlaUtils.cmake
@@ -1,74 +1,74 @@
 # Once done these will be defined:
 #
-# CARLAUTILS_FOUND CARLAUTILS_INCLUDE_DIRS CARLAUTILS_LIBRARIES
+# CarlaUtils_FOUND CarlaUtils_INCLUDE_DIRS CarlaUtils_LIBRARIES
 
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_CARLAUTILS QUIET carla-utils)
+  pkg_check_modules(PC_CarlaUtils QUIET carla-utils)
 endif()
 
 find_path(
-  CARLAUTILS_INCLUDE_DIR
+  CarlaUtils_INCLUDE_DIR
   NAMES utils/CarlaBridgeUtils.hpp
-  HINTS ${PC_CARLAUTILS_INCLUDE_DIRS}
+  HINTS ${PC_CarlaUtils_INCLUDE_DIRS}
   PATHS /usr/include/carla /usr/local/include/carla
   PATH_SUFFIXES carla
   DOC "carla include directory")
 
 find_library(
-  CARLAUTILS_LIBRARY
+  CarlaUtils_LIBRARY
   NAMES carla_utils libcarla_utils
-  HINTS ${PC_CARLAUTILS_LIBRARY_DIRS}
+  HINTS ${PC_CarlaUtils_LIBRARY_DIRS}
   PATHS /usr/lib/carla /usr/local/lib/carla /app/lib/carla
   PATH_SUFFIXES carla)
 
 find_program(
-  CARLAUTILS_BRIDGE_NATIVE
+  CarlaUtils_BRIDGE_NATIVE
   NAMES carla-bridge-native
-  HINTS ${PC_CARLAUTILS_LIBRARY_DIRS}
+  HINTS ${PC_CarlaUtils_LIBRARY_DIRS}
   PATHS /usr/lib/carla /usr/local/lib/carla /app/bin
   PATH_SUFFIXES carla)
 
 find_program(
-  CARLAUTILS_DISCOVERY_NATIVE
+  CarlaUtils_DISCOVERY_NATIVE
   NAMES carla-discovery-native
-  HINTS ${PC_CARLAUTILS_LIBRARY_DIRS}
+  HINTS ${PC_CarlaUtils_LIBRARY_DIRS}
   PATHS /usr/lib/carla /usr/local/lib/carla /app/bin
   PATH_SUFFIXES carla)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   CarlaUtils
-  FOUND_VAR CARLAUTILS_FOUND
-  REQUIRED_VARS CARLAUTILS_INCLUDE_DIR CARLAUTILS_LIBRARY CARLAUTILS_BRIDGE_NATIVE CARLAUTILS_DISCOVERY_NATIVE)
-mark_as_advanced(CARLAUTILS_INCLUDE_DIR CARLAUTILS_LIBRARY CARLAUTILS_BRIDGE_NATIVE CARLAUTILS_DISCOVERY_NATIVE)
+  FOUND_VAR CarlaUtils_FOUND
+  REQUIRED_VARS CarlaUtils_INCLUDE_DIR CarlaUtils_LIBRARY CarlaUtils_BRIDGE_NATIVE CarlaUtils_DISCOVERY_NATIVE)
+mark_as_advanced(CarlaUtils_INCLUDE_DIR CarlaUtils_LIBRARY CarlaUtils_BRIDGE_NATIVE CarlaUtils_DISCOVERY_NATIVE)
 
-if(CARLAUTILS_FOUND)
-  set(CARLAUTILS_INCLUDE_DIRS ${CARLAUTILS_INCLUDE_DIR} ${CARLAUTILS_INCLUDE_DIR}/includes
-                              ${CARLAUTILS_INCLUDE_DIR}/utils)
-  set(CARLAUTILS_LIBRARIES ${CARLAUTILS_LIBRARY})
+if(CarlaUtils_FOUND)
+  set(CarlaUtils_INCLUDE_DIRS ${CarlaUtils_INCLUDE_DIR} ${CarlaUtils_INCLUDE_DIR}/includes
+                              ${CarlaUtils_INCLUDE_DIR}/utils)
+  set(CarlaUtils_LIBRARIES ${CarlaUtils_LIBRARY})
 
   if(NOT TARGET carla::utils)
-    if(IS_ABSOLUTE "${CARLAUTILS_LIBRARIES}")
+    if(IS_ABSOLUTE "${CarlaUtils_LIBRARIES}")
       add_library(carla::utils UNKNOWN IMPORTED GLOBAL)
-      set_target_properties(carla::utils PROPERTIES IMPORTED_LOCATION "${CARLAUTILS_LIBRARIES}")
+      set_target_properties(carla::utils PROPERTIES IMPORTED_LOCATION "${CarlaUtils_LIBRARIES}")
     else()
       add_library(carla::utils INTERFACE IMPORTED GLOBAL)
-      set_target_properties(carla::utils PROPERTIES IMPORTED_LIBNAME "${CARLAUTILS_LIBRARIES}")
+      set_target_properties(carla::utils PROPERTIES IMPORTED_LIBNAME "${CarlaUtils_LIBRARIES}")
     endif()
 
-    set_target_properties(carla::utils PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CARLAUTILS_INCLUDE_DIRS}")
+    set_target_properties(carla::utils PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CarlaUtils_INCLUDE_DIRS}")
   endif()
 
   if(NOT TARGET carla::bridge-native)
     add_executable(carla::bridge-native IMPORTED GLOBAL)
-    set_target_properties(carla::bridge-native PROPERTIES IMPORTED_LOCATION "${CARLAUTILS_BRIDGE_NATIVE}")
+    set_target_properties(carla::bridge-native PROPERTIES IMPORTED_LOCATION "${CarlaUtils_BRIDGE_NATIVE}")
     add_dependencies(carla::utils carla::bridge-native)
   endif()
 
   if(NOT TARGET carla::discovery-native)
     add_executable(carla::discovery-native IMPORTED GLOBAL)
-    set_target_properties(carla::discovery-native PROPERTIES IMPORTED_LOCATION "${CARLAUTILS_DISCOVERY_NATIVE}")
+    set_target_properties(carla::discovery-native PROPERTIES IMPORTED_LOCATION "${CarlaUtils_DISCOVERY_NATIVE}")
     add_dependencies(carla::utils carla::discovery-native)
   endif()
 endif()
